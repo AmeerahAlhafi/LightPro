10f12c01c09dde71a85fb680afa93f0fd7b0a1ad
AmeerahAlhafi <75354682+AmeerahAlhafi@users.noreply.github.com>
2023-01-25 08:50:49 +0300

    Initial Commit

---
+++ LightPro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LightPro
+//
+//  Created by أميرة الحافي on 03/07/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
